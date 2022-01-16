4e45a0e8aa2f7308d41376dde0993f9af83490cc
AbdulazizAlhumam <86843620+AbdulazizAlhumam@users.noreply.github.com>
2022-01-16 18:47:55 +0300

    big big update

---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("%cFinished page", "font-size: 40px; color: lightgreen;")
+console.log("%c@copyright by Abdulaziz Alhumam", "font-size: 10px")
---
+++ skills/script.js
@@ -0,0 +1,2 @@
+console.log("%cAlmost finished page", "font-size: 40px; color: darkgreen;")
+console.log("%c@copyright by Abdulaziz Alhumam", "font-size: 10px")
